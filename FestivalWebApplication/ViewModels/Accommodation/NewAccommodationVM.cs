54fc5033c458ca97ff7dee669205f5fb0a58efee
aneskurtovic <anes.kurtovic@edu.fit.ba>
2020-01-24 00:24:58 +0100

    New performance VM

---
+++ FestivalWebApplication/ViewModels/Accommodation/NewAccommodationVM.cs
@@ -0,0 +1,11 @@
+﻿namespace FestivalWebApplication.ViewModels.Accommodation
+{
+    public class NewAccommodationVM
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public float Distance { get; set; }
+        public int? ImageID { get; set; }
+        public string Description { get; set; }
+    }
+}
