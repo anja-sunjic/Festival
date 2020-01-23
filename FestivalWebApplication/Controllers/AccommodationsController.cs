21d5b31fb2d7382d75ca1cd82ad0d0537cd0517f
aneskurtovic <anes.kurtovic@edu.fit.ba>
2020-01-24 00:18:32 +0100

    Accomodation controller

---
+++ FestivalWebApplication/Controllers/AccommodationsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClassLibrary.Models;
+
+namespace FestivalWebApplication.Controllers
+{
+    public class AccommodationsController : Controller
+    {
+        private readonly FestivalContext _context;
+
+        public AccommodationsController(FestivalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accommodations
+        public async Task<IActionResult> Index()
+        {
+            var festivalContext = _context.Accommodation.Include(a => a.Image);
+            return View(await festivalContext.ToListAsync());
+        }
+
+        // GET: Accommodations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var accommodation = await _context.Accommodation
+                .Include(a => a.Image)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (accommodation == null)
+            {
+                return NotFound();
+            }
+
+            return View(accommodation);
+        }
+
+        // GET: Accommodations/Create
+        public IActionResult Create()
+        {
+            ViewData["ImageID"] = new SelectList(_context.Image, "Id", "Id");
+            return View();
+        }
+
+        // POST: Accommodations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,PhoneNumber,Distance,ImageID,Description")] Accommodation accommodation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(accommodation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ImageID"] = new SelectList(_context.Image, "Id", "Id", accommodation.ImageID);
+            return View(accommodation);
+        }
+
+        // GET: Accommodations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var accommodation = await _context.Accommodation.FindAsync(id);
+            if (accommodation == null)
+            {
+                return NotFound();
+            }
+            ViewData["ImageID"] = new SelectList(_context.Image, "Id", "Id", accommodation.ImageID);
+            return View(accommodation);
+        }
+
+        // POST: Accommodations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,PhoneNumber,Distance,ImageID,Description")] Accommodation accommodation)
+        {
+            if (id != accommodation.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(accommodation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccommodationExists(accommodation.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ImageID"] = new SelectList(_context.Image, "Id", "Id", accommodation.ImageID);
+            return View(accommodation);
+        }
+
+        // GET: Accommodations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var accommodation = await _context.Accommodation
+                .Include(a => a.Image)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (accommodation == null)
+            {
+                return NotFound();
+            }
+
+            return View(accommodation);
+        }
+
+        // POST: Accommodations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var accommodation = await _context.Accommodation.FindAsync(id);
+            _context.Accommodation.Remove(accommodation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccommodationExists(int id)
+        {
+            return _context.Accommodation.Any(e => e.ID == id);
+        }
+    }
+}
