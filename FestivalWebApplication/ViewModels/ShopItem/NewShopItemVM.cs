bc817e852309f0ea10549362ce1a15ab299c19bf
aneskurtovic <anes.kurtovic@edu.fit.ba>
2020-01-24 02:00:46 +0100

    Shop item Views

--- FestivalWebApplication/ViewModels/ShopItem/NewShopItemVM.cs
+++ FestivalWebApplication/ViewModels/ShopItem/NewShopItemVM.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FestivalWebApplication.ViewModels.ShopItem
+﻿namespace FestivalWebApplication.ViewModels.ShopItem
 {
     public class NewShopItemVM
     {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
     }
 }
