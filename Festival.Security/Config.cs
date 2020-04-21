c2f3abf657edbec79f9eb2f408d367bbc96e6560
aneskurtovic <anes.kurtovic@edu.fit.ba>
2020-04-21 21:11:52 +0200

    Microsoft Identity updated

---
+++ Festival.Security/Config.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Festival.Security
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    ClientName = "Festival MVC",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+                    RedirectUris = { "https://127.0.0.1:44330/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://127.0.0.1:44330/signout-callback-oidc" },
+                    AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile }
+                }
+            };
+    }
+}
