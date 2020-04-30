b184998791d3c85f3aa2a2fc7cb6f25b6d12d0a4
Anja <anja.sunjic@gmail.com>
2020-04-30 17:41:44 +0200

    Stage/Detail 140

---
+++ Festival.Web/ViewModels/Stage/DetailStageVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Festival.Web.ViewModels.Stage
+{
+    public class DetailStageVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string SponsorName { get; set; }
+    }
+}
