d771564d56bbb0204033647b3a3b223435863851
Anja <anja.sunjic@gmail.com>
2020-05-03 11:23:12 +0200

    Performance style readjustment 147

--- Festival.Web/ViewModels/Performance/DetailPerformanceVM.cs
+++ Festival.Web/ViewModels/Performance/DetailPerformanceVM.cs
@@ -11,7 +11,7 @@ namespace FestivalWebApplication.ViewModels.Performance
     public class DetailPerformanceVM
     {
         public int ID { get; set; }
-        public DateTime Start { get; set; }
+        public string Start { get; set; }
         public string StageName { get; set; }
         public string PerformerName { get; set; }
         public string PerformerPicture { get; set; }
