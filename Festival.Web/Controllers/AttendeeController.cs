97705a14de40848acc6638eca55a95ac09bdcafe
Anja <anja.sunjic@gmail.com>
2020-05-06 20:27:42 +0200

    Posjetitelj - registracija

--- Festival.Web/Controllers/AttendeeController.cs
+++ Festival.Web/Controllers/AttendeeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Festival.Data.Models;
 using Festival.Data.Repositories;
 using Festival.Web.ViewModels.Attendee;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,39 @@ namespace Festival.Web.Controllers
                     Email = a.Email
                 }).ToList();
             return View(model);
+        }
+        public IActionResult New()
+        {
+            NewAttendeeVM model = new NewAttendeeVM();
+
+            return View(model);
+        }
+        public IActionResult SaveNew(NewAttendeeVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("New");
+            }
+            UserAccount userAccount = new UserAccount
+            {
+                Username = model.Username,
+                Password = model.Password,
+                Type = AccountType.attendee
+            };
+            _repo.AddUserAccount(userAccount);
+
+            Attendee attendee = new Attendee
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber,
+                UserAccountID=userAccount.ID
+
+            };
+            _repo.Add(attendee);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Festival.Web/ViewModels/Attendee/NewAttendeeVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Festival.Web.ViewModels.Attendee
+{
+    public class NewAttendeeVM
+    {
+        [Required(ErrorMessage = "First name is required, please insert it.")]
+        public string FirstName { get; set; } 
+
+        [Required(ErrorMessage = "Last name is required, please insert it.")]
+        public string LastName { get; set; }
+        
+        [Required(ErrorMessage = "Email is required, please insert it.")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Phone number is required, please insert it.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Username is required, please insert the amount.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Promo text for the performer is required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Your password and confirm password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
