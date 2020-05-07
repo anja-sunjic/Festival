c7ff11c08885c43d9faa6887fcd477ceaee9c185
Anja <anja.sunjic@gmail.com>
2020-05-07 02:29:31 +0200

    PDF and email helpers

---
+++ Festival.Web/Helper/Email.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Festival.Web.Helper
+{
+    public class Email
+    {
+        void SendEmail()
+        {
+            using (MailMessage emailMessage = new MailMessage())
+            {
+                emailMessage.From = new MailAddress("account2@gmail.com", "Account2");
+                emailMessage.To.Add(new MailAddress("account1@gmail.com", "Account1"));
+                emailMessage.Subject = "SUBJECT";
+                emailMessage.Body = "BODY";
+                emailMessage.Priority = MailPriority.Normal;
+                using (SmtpClient MailClient = new SmtpClient("smtp.gmail.com", 587))//or 465 if 587 is not working
+                {
+                    MailClient.EnableSsl = true;
+                    MailClient.Credentials = new System.Net.NetworkCredential("account2@gmail.com", "password");
+                    MailClient.Send(emailMessage);
+                }
+            }
+        }
+    }
+}
---
+++ Festival.Web/Helper/PDF.cs
@@ -0,0 +1,27 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Festival.Web.Helper
+{
+    public class PDF
+    {
+        public byte[] CreatePdf()
+        {
+            var stream = new MemoryStream();
+            var writer = new PdfWriter(stream);
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf);
+
+            document.Add(new Paragraph("Hello world!"));
+            document.Close();
+
+            return stream.ToArray();
+        }
+    }
+}
