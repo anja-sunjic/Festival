d6e6a40d03a3b108e24ea610934e660091f8c52d
Anja <anja.sunjic@gmail.com>
2020-05-08 02:00:39 +0200

    Buy Tickets Page

--- Festival.Web/Controllers/TicketTypeController.cs
+++ Festival.Web/Controllers/TicketTypeController.cs
@@ -19,12 +19,13 @@ namespace FestivalWebApplication.Controllers
         }
         public IActionResult Index()
         {
-            List<TicketsVM> model = _repo.GetAll().Select(p => new TicketsVM
+            List<TicketsVM> model = _repo.GetAll().OrderByDescending(p=> p.Tier).Select(p => new TicketsVM
             {
                 Id = p.ID,
                 Name = p.Name,
                 Price = p.Price,
-                Description = p.Description
+                Description = p.Description,
+                Tier=p.Tier
             }).ToList();
 
             return View(model);
--- Festival.Web/ViewModels/TicketType/TicketsVM .cs
+++ Festival.Web/ViewModels/TicketType/TicketsVM .cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public float Price { get; set; }
         public string Description { get; set; }
-        
+        public int Tier { get; set; }
 
     }
 }
