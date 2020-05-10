6ee3454c18128273e0880a45849b3c2114976635
Anja <anja.sunjic@gmail.com>
2020-05-10 16:55:38 +0200

    Guest/Accommodation

---
+++ Festival.Web/Areas/Guest/Controllers/AccommodationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Festival.Data.Repositories;
+using Festival.Web.Areas.Guest.ViewModels.Accommodation;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Festival.Web.Areas.Guest.Controllers
+{
+    [Area("Guest")]
+    public class AccommodationController : Controller
+    {
+        private readonly IAccommodationRepository _repo;
+
+        public AccommodationController(IAccommodationRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            List<AccommodationListVM> Model = _repo.GetAll().Select(a => new AccommodationListVM
+            {
+                ID = a.ID,
+                Address = a.Address,
+                Description = a.Description,
+                Distance = a.Distance,
+                Name = a.Name,
+                PhoneNumber = a.PhoneNumber,
+                Picture = a.Picture
+            }).ToList();
+
+            return View(Model);
+        }
+    }
+}
---
+++ Festival.Web/Areas/Guest/ViewModels/Accommodation/AccommodationListVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Festival.Web.Areas.Guest.ViewModels.Accommodation
+{
+        public class AccommodationListVM
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string PhoneNumber { get; set; }
+            public float Distance { get; set; }
+            public string Description { get; set; }
+            public string Address { get; set; }
+            public string Picture { get; set; }
+        }
+    
+}
